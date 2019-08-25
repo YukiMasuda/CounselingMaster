6f7ea0c9c2c1e586c88b62a5fe965b5d8cb07c44
YukiMasuda <yukimasuda21@gmail.com>
2019-08-25 17:25:08 +0900

    Initial Commit

---
+++ CounselingMaster/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CounselingMaster
+//
+//  Created by 増田悠希 on 2019/08/25.
+//  Copyright © 2019 Yuki Masuda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
